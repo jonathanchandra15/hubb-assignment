fa45154245fd2508c4051904d7be45f18c9c4205
Jonathan Chandra <jonathanjocan@gmail.com>
2021-04-22 00:59:48 +0700

    Add custom tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,10 +2,24 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      'orange': '#FFC865',
+      'dark-blue': '#1F2F60',
+      'white': '#FFFFFF',
+    },
+    fontFamily: {
+      'sans': ['Sora']
+    },
+    fontSize: {
+      'product-button-text': '0.625rem',
+      'xs': '.75rem',
+      'sm': '.875rem',
+      'base': '1rem',
+    }
   },
   variants: {
     extend: {},
   },
   plugins: [],
+  important: true,
 }
